9b33a9f82b59c8026191b79b7cf2bdda700ab7c0
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-07 13:25:16 +0430

    cleanup the main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
 int main() {
-  auto result = (10 <=> 20) > 0;
-  std::cout << result << std::endl;
   std::cout << "Hello world c++ from vscode" << std::endl;
   return 0;
 }
