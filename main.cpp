69191eabdbeb190b0d6b034603f3192163dd4bbf
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-07 13:24:31 +0430

    initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  auto result = (10 <=> 20) > 0;
+  std::cout << result << std::endl;
+  std::cout << "Hello world c++ from vscode" << std::endl;
+  return 0;
+}
