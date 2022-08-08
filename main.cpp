1dc44e16e441d71bc9fb98c13b08eda764c1419c
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-08 18:01:43 +0430

    trying to initialize glfw

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include "gl/glew.h"
+#include "glfw/glfw3.h"
+
 using namespace std;
 
+const GLint WIDTH = 800;
+const GLint HEIGHT = 600;
+
 int main() {
-  cout << "Hello world" << endl;
+  if (!glfwInit()) {
+    cout << "Failed to initialize GLFW" << endl;
+    glfwTerminate();
+    return 1;
+  }
+
+  // set version to 3.3
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+  // we dont want to use deprecated features of OpenGL & Allow forward compatibility
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  // create the main window
+  GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "OPEN_GL", NULL, NULL);
+  if (!mainWindow) {
+    cout << "Failed to create GLFW window" << endl;
+    glfwTerminate();
+    return 1;
+  }
+
+  cout << "GLFW initialized" << endl;
   return 0;
 }
