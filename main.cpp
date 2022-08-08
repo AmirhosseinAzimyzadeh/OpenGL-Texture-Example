432be23fce0324968206a027a855ab45d60f46ca
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-08 09:35:52 +0430

    test external libs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "external_libs/glew.h"
+#include "external_libs/glfw.h"
 
 int main() {
   std::cout << "Hello world c++ from vscode" << std::endl;
+  std::cout << "Adding GLEW and GLFW" << std::endl;
   return 0;
 }
