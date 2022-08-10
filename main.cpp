a3f3e93866dc201efcbed6d76e8e03cfc36dd062
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-10 18:17:26 +0430

    add shaders and createTriangle function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string.h>
+
 #include "gl/glew.h"
 #include "glfw/glfw3.h"
 
@@ -6,6 +8,63 @@ using namespace std;
 
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
+
+// VAOs and VBOs ids
+GLuint VAO;
+GLuint VBO;
+GLuint shader;
+
+// create vertex shader
+static const char* vShader = "\n\
+#version 330 core\n\
+\n\
+layout (location = 0) in vec3 position;\n\
+\n\
+void main()\n\
+{\n\
+    gl_Position = vec4(0.5 * position.x, 0.5 * position.y, position.z, 1.0);\n\
+}";
+
+static const char* fShader = "\n\
+#version 330 core\n\
+\n\
+out vec4 color;\n\
+\n\
+void main()\n\
+{\n\
+    color = vec4(0.1, 0.0, 1.0, 1.0);\n\
+}";
+
+void createTriangle() {
+  GLfloat vertices[] = {
+    -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    0.0f, 1.0f, 0.0f
+  };
+
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER,  sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(
+    0,
+    3,
+    GL_FLOAT,
+    GL_FALSE, // is dynamic?
+    0, // skip
+    0 // offset
+  );
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // unbind VAO
+  glBindVertexArray(0);
+}
+
 
 int main() {
   if (!glfwInit()) {
@@ -57,7 +116,7 @@ int main() {
     glfwPollEvents();
 
     // clear window
-    glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     // swap buffers (what user sees with what we draw)
     glfwSwapBuffers(mainWindow);
