df195b08377c915231c7ac0f67d4752e8d5b9943
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-12 12:07:38 +0430

    replace conan settings with python file

---
+++ conanfile.py
@@ -0,0 +1,9 @@
+from conans import ConanFile, CMake
+
+class PocoTimerConan(ConanFile):
+   settings = "os", "compiler", "build_type", "arch", "compiler"
+   requires = "glfw/3.3.7", "glew/2.2.0", "glm/cci.20220420"
+   generators = "cmake"
+
+  #  def configure(self):
+  #     del self.settings.compiler.libcxx
