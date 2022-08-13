f7cd82cfb52c4a7237778f1d21daba672d40f3dc
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-13 15:53:24 +0430

    add rotations

--- main.cpp
+++ main.cpp
@@ -210,6 +210,7 @@ int main() {
     // creating transform matrix
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, glm::vec3(triangleOffset, triangleOffset, 0.0f));
+    model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 
     glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
