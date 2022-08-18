a74267dfb4bdeb097caa226c4ab13778a2356a0b
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-18 20:16:24 +0430

    initial impl.

---
+++ classes/utils.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <filesystem>
+#include <unistd.h>
+
+namespace utils {
+  // reading a file
+  std::string readFile(const char* filePath) {
+    std::string content;
+    std::string line;
+
+    std::cout << "READING FILE:" << filePath << '\n';
+
+    std::ifstream targetFile (filePath);
+    if (targetFile.is_open()) {
+      char c;
+      while (targetFile.get(c)) {
+        content.push_back(c);
+      }
+    } else {
+      std::cout << "Unable to open file";
+    }
+
+    std::cout << "FILE: --" << content << "\n";
+    return content;
+  }
+}
+
