2c5ea4c8f7b9b8a506d1a6f440da156ca2cc2c1b
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-21 15:12:27 +0430

    implement update function

--- classes/Camera.cpp
+++ classes/Camera.cpp
@@ -4,12 +4,27 @@ Camera::Camera(
   glm::vec3 startPosition,
   glm::vec3 startUp,
   GLfloat startYaw,
-  GLfloat startPitch
+  GLfloat startPitch,
+  GLfloat startMoveSpeed,
+  GLfloat startTurnSpeed
 ) {
   position = startPostion;
   up = startUp;
   yaw = startYaw;
   pitch = startPitch;
+  front = glm::vec3(0.0f, 0.0f, -1.0f);
+
+  moveSpeed = startMoveSpeed;
+  turnSpeed = startTurnSpeed;
+
+  update();
+}
+
+void Camera::update() {
+  front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+  front.y = sin(glm::radians(pitch));
+  front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+  front = glm::normalize(front);
 }
 
 Camera::~Camera() {
