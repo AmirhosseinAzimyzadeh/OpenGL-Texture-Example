5ec34831681eaff48df6b6e3cbe1fdacf166c3cb
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-21 14:54:06 +0430

    initial impl.

---
+++ classes/Camera.h
@@ -0,0 +1,19 @@
+#include <gl/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Camera {
+  public:
+    Camera();
+    ~Camera();
+  
+  private:
+    glm::vec3 position;
+    glm::vec3 front;
+    glm::vec3 up;
+    glm::vec3 right;
+    glm::vec3 worldUp;
+
+    GLfloat yaw;
+    GLfloat pitch;
+};
