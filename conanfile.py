0f6d62740a6438eb2543b2c30b9e2d0a59581825
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-23 10:35:48 +0430

    add stb

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class PocoTimerConan(ConanFile):
    settings = "os", "compiler", "build_type", "arch", "compiler"
-   requires = "glfw/3.3.7", "glew/2.2.0", "glm/cci.20220420"
+   requires = "glfw/3.3.7", "glew/2.2.0", "glm/cci.20220420", "stb/cci.20210910"
    generators = "cmake"
 
   #  def configure(self):
