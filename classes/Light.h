55306cf2bca026deedc7e2c29c3798a7484970b5
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-24 17:21:44 +0430

    add constructor and use light functions

--- classes/Light.h
+++ classes/Light.h
@@ -1,6 +1,15 @@
+#include "glm/glm.hpp"
+#include "gl/glew.h"
+
 class Light {
   public:
     Light();
+    Light(GLfloat red, GLfloat green, GLfloat blue, GLfloat intensity);
     ~Light();
 
+    useLight(GLfloat ambientIntensityLocation, GLfloat ambientColourLocation);
+
+  private:
+    glm::vec3 color;
+    GLfloat ambientIntensity;
 };
