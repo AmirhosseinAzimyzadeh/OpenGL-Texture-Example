3419d6a6b578d84a703648a55946b1e04a67e244
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-08-25 09:51:38 +0430

    add diffuse light properties

--- classes/Light.cpp
+++ classes/Light.cpp
@@ -5,16 +5,36 @@ using namespace std;
 Light::Light() {
   color = glm::vec3(1.0f, 1.0f, 1.0f);
   ambientIntensity = 1.0f;
+  direction = glm::vec3(0.0f, -1.0f, 0.0f);
+  diffuseIntensity = 1.0f;
 }
 
-Light::Light(GLfloat red, GLfloat green, GLfloat blue, GLfloat intensity) {
+Light::Light(
+  GLfloat red,
+  GLfloat green,
+  GLfloat blue,
+  GLfloat intensity,
+  GLfloat xDir,
+  GLfloat yDir,
+  GLfloat zDir,
+  GLfloat diffuseIntensityValue
+) {
   color = glm::vec3(red, green, blue);
   ambientIntensity = intensity;
+  direction = glm::vec3(xDir, yDir, zDir);
+  diffuseIntensity = diffuseIntensityValue;
 }
 
-void Light::useLight(GLfloat ambientIntensityLocation, GLfloat ambientColorLocation) {
+void Light::useLight(
+  GLfloat ambientIntensityLocation,
+  GLfloat ambientColorLocation,
+  GLfloat diffuseIntensityLocation,
+  GLfloat directionLocation
+) {
   glUniform3f(ambientColorLocation, color.x, color.y, color.z);
   glUniform1f(ambientIntensityLocation, ambientIntensity);
+  glUniform1f(diffuseIntensityLocation, diffuseIntensity);
+  glUniform3f(directionLocation, direction.x, direction.y, direction.z);
 }
 
 Light::~Light() {
--- classes/Light.h
+++ classes/Light.h
@@ -4,12 +4,29 @@
 class Light {
   public:
     Light();
-    Light(GLfloat red, GLfloat green, GLfloat blue, GLfloat intensity);
+    Light(
+      GLfloat red,
+      GLfloat green,
+      GLfloat blue,
+      GLfloat intensity,
+      GLfloat xDir,
+      GLfloat yDir,
+      GLfloat zDir,
+      GLfloat diffuseIntensityValue
+    );
     ~Light();
 
-    void useLight(GLfloat ambientIntensityLocation, GLfloat ambientColorLocation);
+    void useLight(
+      GLfloat ambientIntensityLocation,
+      GLfloat ambientColorLocation,
+      GLfloat diffuseIntensityLocation,
+      GLfloat directionLocation
+    );
 
   private:
     glm::vec3 color;
     GLfloat ambientIntensity;
+
+    glm::vec3 direction;
+    Glfloat diffuseIntensity;
 };
